e8cb4f05dc5961c354fa930e8ae6275174f3345f
andyandykim <andyandykim@bitbucket.org>
2022-01-05 09:20:17 -0500

    new file and added UI

--- script.js
+++ script.js
@@ -16,38 +16,40 @@ function playRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection == computerSelection){
+        content.textContent = ("TIE!");
         return "Tie";
     }
     else if (playerSelection == "rock"){
         if (computerSelection == "paper"){
-            console.log("You Lose! Paper beats Rock.");
+            content.textContent = ("You Lose! Paper beats Rock.");
             return 0;
         }
         else {
-            console.log("You win! Rock beats Scissor.");
+            content.textContent = ("You win! Rock beats Scissor.");
             return 1;
         }
     }
     else if (playerSelection == "paper"){
         if (computerSelection == "scissor"){
-            console.log("You Lose! Scissor beats Paper.");
+            content.textContent = ("You Lose! Scissor beats Paper.");
             return 0;
         }
         else {
-            console.log("You win! Paper beats Rock.");
+            content.textContent = ("You win! Paper beats Rock.");
             return 1;
         }
     }
     else if (playerSelection == "scissor"){
         if (computerSelection == "rock"){
-            console.log("You Lose! Rock beats Scissor.");
+            content.textContent = ("You Lose! Rock beats Scissor.");
             return 0;
         }
         else {
-            console.log("You win! Scissor beats Paper.");
+            content.textContent = ("You win! Scissor beats Paper.");
             return 1;
         }
     }
+
 }
 
 function game(){
@@ -56,7 +58,9 @@ function game(){
     let computerChoice = "";
     let playerChoice = "";
     let score = -1;
-    for (let i = 0; i < 5; i++){
+
+    // playing 5 rounds option
+    // for (let i = 0; i < 5; i++){ 
         computerChoice = computerPlay();
         playerChoice = prompt("Enter Rock, Paper or Scissor...");
         score = playRound(playerChoice, computerChoice);
@@ -64,11 +68,14 @@ function game(){
             player++;
             console.log(`Player: ${player} point(s).\nComputer: ${computer} point(s).`);
         }
-        else {
+        else if (score == 0) {
             computer++;
             console.log(`Player: ${player} point(s).\nComputer: ${computer} point(s).`);
         }
-    }
+        else {
+            console.log(`Player: ${player} point(s).\nComputer: ${computer} point(s).`);
+        }
+    // }
     if (player > computer){
         console.log("Player Wins!");
         console.log(`SCORE: Player: ${player} Computer: ${computer} `);
